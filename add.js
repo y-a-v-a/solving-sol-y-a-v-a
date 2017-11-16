2074ea6aedcb7fe26cc4a84876e6ed4c1bb6c3c7
Vincent Bruijn <vebruijn@gmail.com>
2017-11-16 21:58:05 +0100

    update scraper, update index script, adds favicon

--- add.js
+++ add.js
@@ -7,34 +7,41 @@ var path = '/Users/vincentb/Sites/solving-sol';
 var ids = [];
 
 fs.readdir(path, function(err, files) {
-    var i = 0;
 
-    for (; i < files.length; i++) {
-        if (/[0-9]{3,}/.test(files[i])) {
-            (function(i) {
-                fs.readdir(path + '/' + files[i] + '/', function(err, files2) {
-                    var source;
-                    var newDir = __dirname + '/' + files[i];
-                    var j = 0;
-                    var target = newDir + '/index.html';
+  for (let i = 0; i < files.length; i++) {
+    let drawingNumber = files[i];
 
-                    for(; j < files2.length; j++) {
-                        if (/y\-a\-v\-a/.test(files2[j])) {
-                            source = path + '/' + files[i] + '/' + files2[j] + '/index.html';
-                            ids.push(files[i]);
-                            if (!fs.existsSync(newDir)) {
-                                fs.mkdirSync(newDir);
-                                fs.createReadStream(source).pipe(fs.createWriteStream(target));
-                            }
-                        }
-                    }
-                    fs.writeFile(__dirname + '/solutions.json', JSON.stringify(ids, null, 4), function(err) {
-                        if (err) {
-                            throw err;
-                        }
-                    });
-                });
-            }(i));
+    if (/[0-9]{3,}/.test(drawingNumber)) {
+      fs.readdir(`${path}/${drawingNumber}/`, function(err, files2) {
+        var targetDir = __dirname + '/' + drawingNumber;
+
+        for(let j = 0; j < files2.length; j++) {
+          let sourceDir = files2[j];
+
+          if (/y\-a\-v\-a/.test(sourceDir)) {
+            ids.push(drawingNumber);
+
+            fs.readdir(path + '/' + drawingNumber + '/' + sourceDir, function(err, files3) {
+
+              if (!fs.existsSync(targetDir)) {
+                fs.mkdirSync(targetDir);
+              }
+
+              for(let k = 0; k < files3.length; k++) {
+                let fileName = files3[k];
+                let filePath = path + '/' + drawingNumber + '/' + sourceDir + `/${fileName}`;
+                let target = `${targetDir}/${fileName}`;
+                fs.createReadStream(filePath).pipe(fs.createWriteStream(target));
+              }
+            });
+          }
         }
+        fs.writeFile(__dirname + '/solutions.json', JSON.stringify(ids, null, 4), function(err) {
+          if (err) {
+            throw err;
+          }
+        });
+      });
     }
+  }
 });
